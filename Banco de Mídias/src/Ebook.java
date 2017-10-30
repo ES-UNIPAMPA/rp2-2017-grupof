3fd36a015385268b04d0150888c5107ca27c4054
Rodrigo Oliveira <roliveira.loki@gmail.com>
2017-10-30 21:40:55 -0200

    Criação de Novas Classes
    Classes catalogo e derivadas criadas
    atributos da classe midia e ebook implementados e seus construtores e gets e sets.

--- Banco de Mídias/src/Ebook.java
+++ Banco de Mídias/src/Ebook.java
@@ -9,5 +9,50 @@
  * @author roliv
  */
 public class Ebook extends Midia {
+    String genero;
+    String local;
+    String editora;
+    int numPaginas;
+
+    public Ebook(String genero, String local, String editora, int numPaginas, String CaminhoDoArquivo, String titulo, String descricao, String autores, int ano) {
+        super(CaminhoDoArquivo, titulo, descricao, autores, ano);
+        this.genero = genero;
+        this.local = local;
+        this.editora = editora;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+    
     
 }
--- Banco de Mídias/src/Midia.java
+++ Banco de Mídias/src/Midia.java
@@ -9,5 +9,58 @@
  * @author roliv
  */
 public class Midia {
-    
+    String CaminhoDoArquivo;
+    String titulo;
+    String descricao;
+    String autores;
+    int ano;
+
+    public Midia(String CaminhoDoArquivo, String titulo, String descricao, String autores, int ano) {
+        this.CaminhoDoArquivo = CaminhoDoArquivo;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.autores = autores;
+        this.ano = ano;
+    }
+
+    public String getCaminhoDoArquivo() {
+        return CaminhoDoArquivo;
+    }
+
+    public void setCaminhoDoArquivo(String CaminhoDoArquivo) {
+        this.CaminhoDoArquivo = CaminhoDoArquivo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
 }
